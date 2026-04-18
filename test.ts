e6dc3143195e96d06db067c08a441290d20b4273
Tom Leyssens <tleyssens@users.noreply.github.com>
2026-04-18 11:05:57 +0200

    Delete test.ts

--- test.ts
+++
@@ -1,5 +0,0 @@
-import { LatLon } from 'geodesy/utm.js';
-        console.log('test')
-        const latLongP = new LatLon(52.2, 0.12);
-        const utmCoord = latLongP.toUtm();
-        console.log(utmCoord.toString()); // '31 N 303189 5787193'
