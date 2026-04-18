1512294f786aa757100a06f9b7201b02a8ed3940
Tom Leyssens <tleyssens@users.noreply.github.com>
2026-04-18 11:05:48 +0200

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const { LatLon } = require ('geodesy/utm.js')
-        console.log('test')
-        const latLongP = new LatLon(52.2, 0.12);
-        const utmCoord = latLongP.toUtm();
-        console.log(JSON.stringify(utmCoord, null, 4)); // '31 N 303189 5787193'
