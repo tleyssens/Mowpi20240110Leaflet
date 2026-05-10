1814ba47f54c025088d801ec1ffda1869cd51443
Tom Leyssens <tleyssens@users.noreply.github.com>
2026-05-10 14:19:38 +0200

    Refactor: StateManager integreren in config.js en socketApi.js

--- bin/config.js
+++ bin/config.js
@@ -1,14 +1,18 @@
 const useSimulation = true; //Zet op false om hardware aan te sturen
 
-var s = require('./settings.json');
+const StateManager = require('../class/StateManager');
+var initialState = require('./settings.json');
 const constants = require('../lib/constants');
 
 const Mower  = useSimulation
   ? require("../lib/MowerLeeg")
   : require("../lib/Mower");
 
+const stateManager = new StateManager(initialState);
+
 module.exports = {
-    s,
+    s: stateManager,           // backward compatibility
+    stateManager,
     constants,
     Mower,
 };
